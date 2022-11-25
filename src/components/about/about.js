1f28060c22caf0b28b508a43ac468434b70b90a7
Avinar Raisi <avinarraisi@gmail.com>
2022-11-25 20:42:29 +0330

    add about component

---
+++ src/components/about/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container col-6">
+      <p className="lead fst-italic">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        deleniti corporis vero quia, itaque voluptatum dolorem omnis tenetur,
+        pariatur, impedit praesentium fuga molestiae. Quisquam consequuntur ad
+        harum quasi sequi asperiores.
+      </p>
+    </div>
+  );
+}
+
+export default About;
