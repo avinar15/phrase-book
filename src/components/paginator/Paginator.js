60c3cbe663a4581fa50525965d6419064d067e07
Avinar Raisi <avinarraisi@gmail.com>
2022-11-27 22:08:04 +0330

    show total on pagination fix pagination style

--- src/components/paginator/Paginator.js
+++ src/components/paginator/Paginator.js
@@ -29,7 +29,18 @@ const Paginator = ({ data, getPageFn }) => {
   }
 
   let pagesDom = [];
-  for (let i = 1; i <= pageCount; i++) {
+
+  let minPage = data.current_page - 2;
+  if (minPage <= 0) {
+    minPage = 1;
+  }
+
+  let maxPage = data.current_page + 2;
+  if (maxPage > data.last_page) {
+    maxPage = data.last_page;
+  }
+
+  for (let i = minPage; i <= maxPage; i++) {
     pagesDom.push(
       <li key={i} className="page-item">
         <button
@@ -59,10 +70,24 @@ const Paginator = ({ data, getPageFn }) => {
     }
     await getPageFn(newPage);
   };
+  const handelStart = async () => {
+    const newPage = (data.current_page = 1);
+    if (data.current_page < 1) {
+      return;
+    }
+    await getPageFn(newPage);
+  };
+  const handelEnd = async () => {
+    const newPage = data.last_page;
+    if (data.last_page < newPage) {
+      return;
+    }
+    await getPageFn(newPage);
+  };
 
   return (
-    <div>
-      <div className="fixed-bottom">
+    <div className="d-flex flex-column align-items-center">
+      <div>
         <nav aria-label="Page navigation example">
           <ul className="pagination justify-content-center">
             <li className={"page-item " + (disabledPrev ? "disabled" : "")}>
@@ -70,7 +95,17 @@ const Paginator = ({ data, getPageFn }) => {
                 Previous
               </button>
             </li>
+            <li className={"page-item " + (disabledPrev ? "d-none" : "")}>
+              <button className="page-link" onClick={handelStart}>
+                ...
+              </button>
+            </li>
             {pagesDom}
+            <li className={"page-item " + (disabledNext ? "d-none" : "")}>
+              <button className="page-link" onClick={handelEnd}>
+                ...
+              </button>
+            </li>
             <li className={"page-item " + (disabledNext ? "disabled" : "")}>
               <button className="page-link" onClick={handelnext}>
                 Next
@@ -79,6 +114,9 @@ const Paginator = ({ data, getPageFn }) => {
           </ul>
         </nav>
       </div>
+      <span className="fst-italic fw-lighter">
+        {data.from}-{data.to} from {data.total}
+      </span>
     </div>
   );
 };
